aa2eb2eb76ca4c7a537c02f117f754670ec7be1d
Bel Hadj Hammouda Imene <bel.hadj.hammouda.imene@gmail.com>
2021-05-18 14:40:06 +0000

    DML

---
+++ DML.sql
@@ -0,0 +1,11 @@
+-- Insert values in Customers table
+INSERT INTO customers VALUES (‘C01’, ’Ali’, ‘71321009’)
+INSERT INTO customers VALUES (‘C02’, ’ASMA’, ‘77345823’)
+
+-- Insert values in Product table
+INSERT INTO product VALUES (‘P01’, ‘Samsung Galaxy S20’, ‘Smartphone’, 3299)
+INSERT INTO product VALUES (‘P02’, ‘Asus Notebook’, ‘pc’, 4599)
+
+-- Insert values in Orders table
+INSERT INTO orders VALUES (‘C01‘, P02’, ‘Null’, 2, 9198)
+INSERT INTO orders VALUES (‘C02‘, P01’, ‘28/05/2020’, 1, 3299)
